7aee4c6397d9434c001e3b829b78c72a06f34167
lix <419940957@qq.com>
2017-10-26 17:32:48 +0800

    更新主页控制器部分代码

--- src/main/java/com/lix/controller/IndexController.java
+++ src/main/java/com/lix/controller/IndexController.java
@@ -2,21 +2,39 @@ package com.lix.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.lix.service.BlogService;
+
+/**
+ * 主页控制器
+ * 
+ * @author lix
+ *
+ */
 @RequestMapping("/")
 @Controller
 public class IndexController {
+    
+    @Autowired
+    private BlogService blogService;
 
-	@RequestMapping("/index")
-	public String index(ModelMap map, @RequestParam(value = "page", required = false) String page,
-			@RequestParam(value = "typeId", required = false) String typeId,
-			@RequestParam(value = "releaseDateStr", required = false) String releaseDateStr, ModelMap modelMap,
-			HttpServletRequest request) {
-		return null;
-	}
+    @RequestMapping("/index")
+    public String index(ModelMap map, @RequestParam(value = "page", required = false) String page,
+            @RequestParam(value = "typeId", required = false) String typeId,
+            @RequestParam(value = "releaseDateStr", required = false) String releaseDateStr,
+            ModelMap modelMap, HttpServletRequest request) {
+        
+        if(StringUtils.isEmpty(page)){
+            page = "1";
+        }
+        
+        return null;
+    }
 
 }
--- src/main/java/com/lix/dao/BlogTypeDao.java
+++ src/main/java/com/lix/dao/BlogTypeDao.java
@@ -6,11 +6,19 @@ import com.lix.pojo.BlogType;
 
 import tk.mybatis.mapper.common.Mapper;
 
-public interface BlogTypeDao extends Mapper<BlogType>{
+public interface BlogTypeDao extends Mapper<BlogType> {
     /**
      * 获取所有博客类型名称及其相关数量，并按照博客类别排序编码正序排序
+     * 
      * @return
      */
-	public List<BlogType> queryCountList();
-	
+    public List<BlogType> queryCountList();
+
+    /**
+     * 通过id查询博客类型
+     * 
+     * @return
+     */
+    public BlogType queryBlogTypeById();
+
 }
